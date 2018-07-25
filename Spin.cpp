8cc63212e162be0136d7c46a57608e0827e85760
fsxbhyy <39934605+fsxbhyy@users.noreply.github.com>
2018-07-26 03:26:08 +0800

    Add files via upload

---
+++ Spin.cpp
@@ -0,0 +1,15 @@
+#include "head.h"
+
+Spin::Spin(){
+	int i,j;
+	for(i=0;i<3;i++){
+		this->cor[i]=0;
+		this->ZC[i]=0;
+		this->IP[i]=0;
+		this->vector[i][0]=0;
+		this->vector[i][1]=0;
+		this->vector[i][2]=1;
+	}
+}
+
+
